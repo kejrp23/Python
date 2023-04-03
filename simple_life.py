90efc6d5680b418275da0f722d1dfc70a5c377b0
Jason R. Pittman <j.pittman51@att.net>
2023-04-03 13:02:31 -0400

    Create simple_life.py
    added clear function for screen clearing system independent

---
+++ simple_life.py
@@ -0,0 +1,15 @@
+import os
+import platform
+
+system = platform.system()
+
+def clear():
+  if system == 'Darwin':
+      os.system('cls')
+  elif sysetm == 'Windows':
+    os.system('clear')
+    
+    
+ 
+
+
